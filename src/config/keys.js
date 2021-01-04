f23408a1d51b019f21f9003c9f729fdb525d0e77
jacobmiller22 <jacobmiller22@vt.edu>
2021-01-04 16:27:38 -0500

    configuration change

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,9 +1,10 @@
 let keys = null;
 if (process.env.NODE_ENV !== "production") {
   keys = require("./dev");
+  console.log(keys);
 } else {
   // prod
   keys = require("./prod");
 }
 
-export default keys;
+export default keys.keys;
